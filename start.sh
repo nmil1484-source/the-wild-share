191cf8524a0f32543538cb49088cd0ec284166cf
nmil1484-source <nmil1484@gmail.com>
2025-10-16 00:00:44 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec python3 -m gunicorn --bind 0.0.0.0:$PORT src.main:app
+exec python3 -c "from src.main import app; app.run(host='0.0.0.0', port=int(__import__('os' ).environ.get('PORT', 5000)))"
