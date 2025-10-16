bba2c23854645b68f71ff984aa50216916148150
nmil1484-source <nmil1484@gmail.com>
2025-10-15 23:58:54 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-python3 -m gunicorn --bind 0.0.0.0:$PORT src.main:app
+exec python3 -m gunicorn --bind 0.0.0.0:$PORT src.main:app
