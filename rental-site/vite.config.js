a6f2de4989678f476900e816209193b0ebf82cf0
Wild Share Admin <admin@wildshare.com>
2025-10-31 00:54:19 -0400

    Add hash to filenames for aggressive cache busting

--- rental-site/vite.config.js
+++ rental-site/vite.config.js
@@ -27,6 +27,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`,
         manualChunks: undefined
       }
     }
